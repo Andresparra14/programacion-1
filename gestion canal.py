16fbbda5c83db4a481e5e5eb7ebade3ccb2580c1
Andresparra14 <130109317+Andresparra14@users.noreply.github.com>
2023-04-22 19:14:54 -0500

    Update gestion canal.py

--- gestion canal.py
+++ gestion canal.py
@@ -1,104 +1,86 @@
-#gestion canal
-canal = {}
-nombre=input("digite el nombre de su canal: ")
-descripcion=input("ingrese la descripcion de su canal: ")
-categoria=input(""" 
-                    ┌─────────────────────────────────────────────────────────┐
-                    │       elija la categoria de su canal:                   │
-                    │_________________________________________________________│
-                    │                                                         │
-                    │    1- comedia                                           |
-                    |    2- animación                                         |
-                    |    3- belleza                                           |
-                    |    4- moda                                              |
-                    |    5- cocina                                            │
-                    │    6- ASMR(respuesta sensorial meridiana autónoma)      |
-                    |    7- vlogs                                             |
-                    |    8- videojuegos                                       |
-                    |    9- empresas                                          |
-                    │   10- tecnología                                        |
-                    |   11- viajes                                            |
-                    |                                                         |
-                    └─────────────────────────────────────────────────────────┘ 
-                """)
-videos=int(input('ingrese la cantidad de videos que tiene: '))
+import random
+
 visualizaciones=0
-like=0
+likes=0
 seguidores=0
 tiempo=0
 video=0
+ganancias=0
+# Gestion del canal
+canal = {}
+nombre=input("Digite el nombre de su canal: ")
+descripcion=input("Ingrese la descripción de su canal: ")
+videos=int(input('Ingrese la cantidad de videos que tiene: '))
+
 while videos > 0:
-    # Información para un video en particular
+    
     video+=1
+    
     print("VIDEO NUMERO ", video)
-    visitas=int(input("cuantas visitas tiene el video?: "))
+    suscripciones_vid=random.randint(250, 2000)
+    visitas = random.randint(1000, 100000)
+    likes_vid = random.randint(1000, 15000)
+    comentarios = random.randint(50, 300)
+    tarifa= random.randint(2,32)
+
+    print("Datos del video subido a YouTube:")
+    print("Suscripciones: ",suscripciones_vid)
+    print("Visitas:", visitas)
+    print("Likes:", likes_vid)
+    print("Comentarios:", comentarios)
+    
+    seguidores+=suscripciones_vid
     visualizaciones+=visitas
-    likes=int(input("cuantas me gusta tiene el video?: "))
-    like+=likes
-    duracion=float(input("cuanto tarda el video tiempo en minutos: "))
+    likes+=likes_vid
+    
+    while True:
+        try:
+            duracion=float(input("Cuánto tarda el video en minutos: "))
+            if duracion > 0:
+                break
+            else:
+                print("ERROR: La duración del video debe ser mayor a 0.")
+        except ValueError:
+            print("ERROR: Ingrese un valor numérico válido para la duración del video.")
+
     segundos=duracion*60
     horas=segundos / 3600
     time=horas*visitas
-    tiempo+=time
-    suscripciones=int(input("¿cuantos seguidores obtubo del video?: "))
-    seguidores+=suscripciones
+    tiempo+=int(time)
+    ganancia = tarifa * (visualizaciones / 1000)
+    ganancias+=ganancia
+    
+    
     videos -= 1
     canal['visitas'] = visualizaciones
-    canal['likes']= like
+    canal['likes']= likes
     canal['tiempo de reproduccion']= tiempo
     canal['suscriptores']= seguidores
-    print("""
-            ┌─────────────────────────────────────────────────────────┐
-            │       Elija cual metodo usa para generar ganacias       │
-            │_________________________________________________________│
-            │                                                         │
-            │    1- Publicidad en los videos                          │
-            │    2- Membresías de YouTube                             │
-            │                                                         │
-            └─────────────────────────────────────────────────────────┘ 
-            """) 
-    opcion=int(input("ingrese la por cual metodo obtiene ganacias responda (1 0 2): "))
+    canal['dinero']= ganancias
     
-    if canal['tiempo de reproduccion'] > 0:
+while True:
+    try:  
+        buscar=input("Qué información desea ver: (1) visitas, (2) likes, (3) tiempo de reproducción, (4) suscriptores, (5) ganancias, (6) salir? ")
 
-        if opcion==1:
-            tarifa=float(input("ingrese la tarifa de youtube por (1000) visualizaciones: "))
-            ganancia1=tarifa*(visualizaciones/1000)
-            print("el total ganado de la publicidad de su canal es: ",ganancia1)
-        elif opcion==2:
-            membresia=float(input("ingrese el precio que usted eligio para la membresia de su canal: "))
-            suscritos=int(input("cuantas personas estan susbcritas a esta membresia: "))
-            if suscritos<=seguidores:
-                ganancia2=(suscritos*membresia)*0.70
-                print("el total ganado de la suscripcion de su canal es: ",ganancia2)
-                exit()
-            else:
-                print("ERROR, ingrese de nuevo los datos")
+        if buscar=="visitas" or buscar=="1":
+            visitas = canal.get('visitas', 0)
+            print(visitas)
+        elif buscar=="likes" or buscar=="2":
+            likes = canal.get('likes', 0)
+            print(likes)
+        elif buscar=="tiempo de reproducción" or buscar=="3":
+            tiempo_rep = canal.get('tiempo de reproduccion', 0)
+            print(tiempo_rep)
+        elif buscar=="suscriptores" or buscar=="4":
+            fans = canal.get('suscriptores', 0)
+            print(fans)
+        elif buscar=="ganancias" or buscar=="5":
+            dinero_USD= canal.get('dinero')
+            print(dinero_USD)
+        elif buscar=="salir" or buscar=="6":
+            print("¡Hasta luego!")
+            break
         else:
-            print("Error intente denuevo")
-    
-
-    
-buscar=input("que informacion desea ver: visistas , likes, tiempo de reproduccion, suscriptores ")
-
-if buscar=="visitas":
-    visitas = canal.get('visitas', 0)
-    print(visitas)
-elif buscar=="likes":
-    likes = canal.get('likes', 0)
-    print(likes)
-elif buscar=="tiempo de reproduccion":
-    tiempo_rep = canal.get('tiempo de reproduccion', 0)
-    print(tiempo_rep)
-elif buscar=="suscriptores":
-    fans = canal.get('suscriptores', 0)
-    print(fans)
-else:
-    print("Opcion invalida")
-print("deseas borrar los datos?")
-
-borrar=input("¿Desea borrar la informacion de la base de datos? si/no ")
-if borrar=="si":
-    canal.clear()
-else:
-    exit()
+            print("ERROR: Opción inválida. Por favor, seleccione una opción válida.")
+    except Exception as e:
+        print("ERROR: Ocurrió un error. Por favor, inténtelo de nuevo. Detalles del error:", str(e))
